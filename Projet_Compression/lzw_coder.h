372c6589531e469a7f1cc0e4b469bff4bfd74f91
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2018-10-25 14:17:53 +0200

    MAJ
    Mise à jour de Lzw

--- Projet_Compression/lzw_coder.h
+++ Projet_Compression/lzw_coder.h
@@ -1,6 +1,6 @@
 /**
-* \file lzw_coder.h
-* \brief en-tete de lzw-coder
+* @file lzw_coder.h
+* @brief en-tete de lzw-coder
 */
 #ifndef DEF_CODAGE
 #define DEF_CODAGE
@@ -8,12 +8,12 @@
 #include"stdio.h"
 
 /**
-* \struct Dictionary_t
-* \brief Dictionnaire de mots
+* @struct Dictionary_t
+* @brief Dictionnaire de mots
 *
 *Dionctionary_t est un objet comportant un 
 *tableau de taille maximum SIZE_MAX avec 
-*size mots qui sont socke dans un 
+*size mots qui sont dans un 
 *tableau de pointeur
 */
 typedef struct {
@@ -21,8 +21,18 @@ typedef struct {
 	int size;
 }Dictionary_t;
 
-typedef struct Array_t{
+/**
+*@struct List_t
+*@brief Liste de code
+*
+*List_t est un objet comportant un code
+*et un pointeur sur un suivant du meme type. 
+*C'est une liste..
+*/
+typedef struct List_t{
 	int code;
-	Array_t * next;
-}Array_t;
+	List_t * next;
+}List_t;
+
+
 #endif
