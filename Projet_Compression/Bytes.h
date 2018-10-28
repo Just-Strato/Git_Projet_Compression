ae1351c7a02d328207c189d902ea2dedd3b76c43
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2018-10-28 10:52:11 +0100

    MAJ

--- Projet_Compression/Bytes.h
+++ Projet_Compression/Bytes.h
@@ -5,19 +5,19 @@
 *byte type definition
 *
 */
-typedef unsigned char t_octet;
+typedef unsigned char Byte_t;
 
 /**
 *definition du type triple d'octet
 *
 */
-typedef t_octet byteTriplet_t[3];
+typedef Byte_t byteTriplet_t[3];
 
 /**
 * @struct BytesTripletArray_t
 * @brief Tableau de triplet
 *
-*Represente un tableau de comportant size
+*Represente un tableau comportant size
 *triple d'octet dans array 
 */
 typedef struct {
@@ -36,8 +36,8 @@ typedef struct {
   * 	+ ident, son identifiant
   * 	+	mode == 1, affiche également la valeur en décimal
   */
-void afficher_octet(
-	const t_octet Oc,
+void displayByte(
+	const Byte_t Oc,
 	const char *ident,
 	const int mode);
 
@@ -45,59 +45,59 @@ void afficher_octet(
  * @brief lire un octet en s'assurant que sa valeur soit bien comprise
  * entre les valeurs 0 et 255 incluses
  */
-t_octet lire_octet();
+Byte_t lire_octet();
 
 
 /**
  * @brief extraire la valeur du n^{ième} bit de l'octet Oc
  * avec n comprise entre 0 et 7
  */
-int extraire_bit(const t_octet Oc, const int n);
+int extraire_bit(const Byte_t Oc, const int n);
 
 
 /**
  * @brief modifier la valeur du n^{ième} bit de l'octet OC
  * avec n compris entre 0 et 7
  */
-t_octet modifier_bit(const t_octet Oc, const int n);
+Byte_t modifier_bit(const Byte_t Oc, const int n);
 
 /**
  *
  * @brief extraire les valeurs des 4 bits de poids faible de l'octet Oc
  */
-t_octet lire_quartetInf(t_octet O);
+Byte_t lire_quartetInf(Byte_t O);
 
 /**
  *
  * @brief extraire les valeurs des 4 bits de poids fort de l'octet Oc
  */
-t_octet lire_quartetSup(t_octet O);
+Byte_t lire_quartetSup(Byte_t O);
 
 /**
  *
  * @brief modifier les valeurs des 4 bits de poids faible de l'octet Oc
  */
-t_octet ecrire_quartetInf(t_octet O, t_octet qi);
+Byte_t ecrire_quartetInf(Byte_t O, Byte_t qi);
 
 /**
  *
  * @brief modifier les valeurs des 4 bits de poids fort de l'octet Oc
  */
-t_octet ecrire_quartetSup(t_octet O, t_octet qs);
+Byte_t ecrire_quartetSup(Byte_t O, Byte_t qs);
 
 /**
 *
 * @brief ecrire x dans la moitie superieur de b et le reste dans a, puis y dans la
 * moitie inferieur de b et le reste dans c
 */
-void ecrire_troisOctets(t_octet *a, t_octet *b, t_octet *c, int x, int y);
+void ecrire_troisOctets(Byte_t *a, Byte_t *b, Byte_t *c, int x, int y);
 
 /**
 *
 * @brief ecrire dans x et y deux entier recuperés dans a et quartet superieur de b,
 * et dans quartet inferieur de b et c
 */
-void recuperer_troisOctets(t_octet a, t_octet b, t_octet c, int *x, int *y);
+void recuperer_troisOctets(Byte_t a, Byte_t b, Byte_t c, int *x, int *y);
 
 /**
 *
@@ -124,11 +124,11 @@ void freeTabTriplesOctet(T_TriplesOctet *x);
 *@brief return chiffré ou dechiffré un octet à partir de la technique XOR avec une
 *clef qui sera entré en paramètre
 */
-t_octet cryptageDecryptageXOR(t_octet octet, t_octet clef);
+Byte_t cryptageDecryptageXOR(Byte_t octet, Byte_t clef);
 
 /**
 *@brief chiffre ou dechiffrechaque octet d'un T_TriplesOctet avec XOR
 */
-void cryptageDecryptageTripletOctet(T_TriplesOctet *tabOctet, t_octet clef);
+void cryptageDecryptageTripletOctet(T_TriplesOctet *tabOctet, Byte_t clef);
 
 #endif
