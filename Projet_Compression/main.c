d6ec4aa5b792805139160b9993923b00d7226c84
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2018-11-01 17:55:42 +0100

    recherche de problème

--- Projet_Compression/main.c
+++ Projet_Compression/main.c
@@ -5,13 +5,27 @@
 
 int main(int argc, char* argv[]) {
 
-	char msg[] = "Bacchanales Bacchus";
+	char msg[] = "Bacchanales Bacchus", *sumD, *sumC;
+	Byte_t key=0x11111111;
 
 	Dictionary_t* diary = allocateDiary();
 
 	CodeArray_t* caray = lzwCoder(diary, msg);
 	
-	printf("%s",summaryCodeArray(caray));
+	printf("%s\n", sumD = summaryCodeArray(diary));
+
+	printf("%s\n", sumC=summaryCodeArray(caray));
+
+	ByteTripletArray_t* bytray = compressCode(caray);
+
+	//encryDecryByteTripletArray(bytray, key);
+
+	releaseDiary(diary);
+	releaseCodeArray(caray);
+	releaseByteTripletArray(bytray);
+
+	free(sumD);
+	free(sumC);
 
 	system("pause");
 	return EXIT_SUCCESS;
